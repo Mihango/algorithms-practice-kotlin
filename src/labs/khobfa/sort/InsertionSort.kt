b73f37f42f17a9f41ff8e8aca47dfad74e4ca99f
Mihango <mihangokennedy@gmail.com>
2020-04-10 13:39:45 +0300

    added insertion sort recursive

--- src/labs/khobfa/sort/InsertionSort.kt
+++ src/labs/khobfa/sort/InsertionSort.kt
@@ -5,7 +5,8 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val data = intArrayOf(20, 35, -15, 7, 55, 1, -22, 5)
     val timeTaken = measureTimeMillis {
-        insertionSort(data)
+        // insertionSort(data)
+        insertionSortRecursive(data, data.size)
     }
     println("Sorted >>>>>>>> ${data.contentToString()} >>>>>>> time take: $timeTaken ")
 }
@@ -21,4 +22,33 @@ fun insertionSort(data: IntArray) {
         }
         data[j + 1] = item
     }
+}
+
+fun insertionSort_2(data: IntArray) {
+    for (firstUnsortedIndex in 1 until data.size) {
+        val newElement = data[firstUnsortedIndex]
+        var i: Int
+        i = firstUnsortedIndex
+        while (i > 0 && data[i - 1] > newElement) {
+            data[i] = data[i - 1]
+            i--
+        }
+        data[i] = newElement
+    }
+}
+
+fun insertionSortRecursive(data: IntArray, items: Int) {
+    if(items < 2) {
+        return
+    }
+
+    insertionSortRecursive(data, items - 1)
+
+    val newElement = data[items-1]
+    var i: Int = items - 1
+    while (i > 0 && data[i - 1] > newElement) {
+        data[i] = data[i - 1]
+        i--
+    }
+    data[i] = newElement
 }
