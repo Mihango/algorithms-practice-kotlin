e924b905535000cca3c6755f2942989f723e92e6
Mihango <mihangokennedy@gmail.com>
2020-04-19 19:52:56 +0300

    added linear serch algorithm

--- src/labs/khobfa/search/LinearSearch.kt
+++ src/labs/khobfa/search/LinearSearch.kt
@@ -1,2 +1,18 @@
 package labs.khobfa.search
 
+fun main() {
+    val data = intArrayOf(20, 35, -15, 7, 55, 1, -22)
+    println("Find -22 >>> ${linearSearch(data, -22)}")
+    println("Out of array 4 >>> ${linearSearch(data, 3)}")
+}
+
+/***
+ * Time complexity id O(n)
+ */
+fun linearSearch(data: IntArray, value: Int): Int {
+    for (i in data.indices) {
+        if(data[i] == value)
+            return i
+    }
+    return -1
+}
